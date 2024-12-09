a0655673f5c21813b6bd5e7ebc9013ff8ceb4c7c
GiolapalmaHub <giovanni.menozzi@prton.me>
2024-12-09 17:41:13 +0100

    abbiamo fatto la prima parte dei semafori

--- asl.c
+++ asl.c
@@ -7,17 +7,48 @@ static struct list_head semd_h;
 void initASL() {
 }
 
+void addToPos(pcb_t* p, semd_t* sem){
+    list_add_tail(&p->p_list, &sem->s_procq);    
+}
+
 int insertBlocked(int* semAdd, pcb_t* p) {
+    //che cazzo sc
+    semd_t * pos;
+
+    list_for_each_entry(pos, &semd_h, s_link){
+        if(pos->s_key == semAdd){
+            addToPos(p, pos);
+            return FALSE; //Probabilmente sbagliato :(
+        }
+    }
+    if (list_empty(&semdFree_h)) return TRUE; //Cazzo
+    
+    struct list_head* newSemList = list_next(&semdFree_h);
+    list_del(newSemList);
+
+    semd_t * newSem = container_of(newSemList, semd_t, s_link);
+    // inizializzazione del semaforo
+    *newSem->s_key = *semAdd;
+    // assumiamo che procQ sia gia inizializzato dalla funzione 
+    // initASL(), altrimenti e' inutile
+    addToPos(p, newSem);
+    list_add_tail(newSemList, &newSem->s_procq);
+
+    return FALSE;
 }
 
 pcb_t* removeBlocked(int* semAdd) {
+    
 }
 
 pcb_t* outBlockedPid(int pid) {
+
 }
 
 pcb_t* outBlocked(pcb_t* p) {
+
 }
 
 pcb_t* headBlocked(int* semAdd) {
+
 }
