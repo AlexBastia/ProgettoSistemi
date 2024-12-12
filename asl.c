efbde81d992a25826ca3ec03021e3088e2130072
GiolapalmaHub <giovanni.menozzi@prton.me>
2024-12-12 17:02:44 +0100

    init ASL

--- asl.c
+++ asl.c
@@ -4,15 +4,27 @@ static semd_t semd_table[MAXPROC];
 static struct list_head semdFree_h;
 static struct list_head semd_h;
 
-void initASL() {
+void initASL() { 
+    // Initialize the semdFree list to contain all the elements of the array static semd_t semdTable[MAXPROC]. This method will be only called once during data structure initialization.
+
+
+    // sentinella 
+    INIT_LIST_HEAD(&semdFree_h);
+
+    for (int i = 0; i < MAXPROC; i++){
+        list_add(&semd_table[i], &semdFree_h);
+    }
+
 }
 
 void addToPos(pcb_t* p, semd_t* sem){
-    list_add_tail(&p->p_list, &sem->s_procq);    
+    list_add_tail(&p->p_list, &sem->s_procq);
 }
 
 int insertBlocked(int* semAdd, pcb_t* p) {
-    //che cazzo sc
+    // Insert the PCB pointed to by p at the tail of the process queue associated with the semaphore whose key is semAdd and set the semaphore address of p to semaphore with semAdd. If the semaphore is currently not active (i.e. there is no descriptor for it in the ASL), allocate a new descriptor from the semdFree list, insert it in the ASL (at the appropriate position), initialize all of the fields (i.e. set s_key to semAdd, and s_procq to mkEmptyProcQ()), and proceed as 4above. If a new semaphore descriptor needs to be allocated and the semdFree list is empty, return TRUE. In all other cases return FALSE. 
+
+
     semd_t * pos;
 
     list_for_each_entry(pos, &semd_h, s_link){
