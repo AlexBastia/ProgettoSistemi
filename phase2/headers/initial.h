e44bbc727621eb13ab6d29e48498f1f392ae58a9
Alex Bastianini <alex.bastianini@studio.unibo.it>
2025-04-08 16:13:23 +0200

    fixino extern

--- phase2/headers/initial.h
+++ phase2/headers/initial.h
@@ -4,11 +4,13 @@
 #include "../../headers/const.h"
 #include "../../headers/types.h"
 /* Global Variables */
-int process_count; /* number of started, but not yet terminated processes */
-struct list_head ready_queue;  /* queue of PCBs in 'ready' state */
-pcb_PTR current_process[NCPU]; /* pointers to the running PCB on each CPU */
-semd_t device_semaphores[SEMDEVLEN]; /* array of semaphores, one for each
+extern int
+    process_count; /* number of started, but not yet terminated processes */
+extern struct list_head ready_queue; /* queue of PCBs in 'ready' state */
+extern pcb_PTR
+    current_process[NCPU]; /* pointers to the running PCB on each CPU */
+extern semd_t device_semaphores[SEMDEVLEN]; /* array of semaphores, one for each
                                         (sub)device */
-volatile unsigned int global_lock;   /* 0 or 1 */
+extern volatile unsigned int global_lock;   /* 0 or 1 */
 
 #endif
