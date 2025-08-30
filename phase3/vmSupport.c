3601f2dda59e33482c174dab71e4d6408baa2fbf
Dario <dariovenuto04@gmail.com>
2025-08-30 02:53:31 +0200

    pager+

--- phase3/vmSupport.c
+++ phase3/vmSupport.c
@@ -11,93 +11,62 @@ static void updateTLB(pteEntry_t* p);
 static int isSwapFrameFree(int frame);
 static int getSwapFrame();
 static int getFifoFrame();
+#define VPN_INDEX(entry_hi) (((entry_hi) >> VPNSHIFT) & 0x1F)  
 
 void pager() {
     support_t* sup = (support_t*)SYSCALL(GETSUPPORTPTR, 0, 0, 0);
     state_t* current_state = &sup->sup_exceptState[PGFAULTEXCEPT];
     unsigned int asid = sup->sup_asid;
     int pid = SYSCALL(GETPROCESSID, 0, 0, 0);
-
-    // 1. Estrai il VPN completo che ha causato il fault
-    unsigned int faulting_vpn = current_state->entry_hi >> VPNSHIFT;
-    // 2. Traduci il VPN nell'indice corretto per la page table
-    unsigned int page_tbl_index;
-    if (faulting_vpn == (USERSTACKTOP >> VPNSHIFT) - 1) {
-        // Caso speciale per la pagina di stack, che è l'ultima della page table
-        page_tbl_index = MAXPAGES - 1;
-    } else {
-        // Per tutte le altre pagine, l'indice è il VPN meno l'indirizzo di partenza
-        page_tbl_index = faulting_vpn - (UPROCSTARTADDR >> VPNSHIFT);
-    }
-
-    // 3. Controlla che l'indice sia valido. Se non lo è, è un segmentation fault.
-    if (page_tbl_index < 0 || page_tbl_index >= MAXPAGES) {
-        programTrapHandler(current_state);
-        return; // Esci dal pager
-    }
-
-    // 4. Ottieni il puntatore corretto alla Page Table Entry usando l'indice calcolato
-    pteEntry_t* pte_p = &sup->sup_privatePgTbl[page_tbl_index];
-
-    // Adesso il resto del codice userà il puntatore corretto 'pte_p' e l'indice 'page_tbl_index'
-
+    unsigned int p = VPN_INDEX(current_state->entry_hi);  
     unsigned int cause = current_state->cause;
     if ((cause & CAUSE_EXCCODE_MASK) == EXC_MOD) {
         programTrapHandler(current_state);
         return;
     };
     getMutex(&swap_pool_sem, pid);
+    pteEntry_t* pte_p = &sup->sup_privatePgTbl[p];
 
+    for (int i = 0; i < POOLSIZE; i++){                                 //6
+        if(swap_pool_table[i].sw_asid==asid && swap_pool_table[i].sw_pageNo == p){
+            CRITICAL_START();
+            updateTLB(swap_pool_table[i].sw_pte);
+            CRITICAL_END();
+            if(sup->sup_privatePgTbl[p].pte_entryLO & ENTRYLO_VALID){
+                releaseMutex(&swap_pool_sem, pid);
+                LDST(current_state);
+            }
+        }
+    }
     int victim = getSwapFrame();
     int page_out_needed = !isSwapFrameFree(victim);
-    int x_asid = 0, k_vpn = 0;
-    pteEntry_t* k_pte = NULL;
 
     if (page_out_needed) {
-        x_asid = swap_pool_table[victim].sw_asid;
-        k_vpn = swap_pool_table[victim].sw_pageNo;
-        k_pte = swap_pool_table[victim].sw_pte;
-    }
-
-    if (page_out_needed) {
-
-        klog_print("vmSupport: Frame occupato da ASID: ");
-        klog_print_dec(x_asid);
-        klog_print(", VPN: ");
-        klog_print_hex(k_vpn);
-        klog_print(". Avvio scrittura su flash (page-out)...\n");
+        int x_asid = swap_pool_table[victim].sw_asid;
+        int k_vpn = swap_pool_table[victim].sw_pageNo;
+        pteEntry_t* k_pte = swap_pool_table[victim].sw_pte;
         k_pte->pte_entryLO &= ~VALIDON;
 
-        CRITICAL_START();
         updateTLB(k_pte);
         read_or_write_flash(victim, k_vpn, x_asid, FLASHWRITE);
         klog_print("vmSupport: Page-out completato.\n");
-        CRITICAL_END();
-
     }
-
-
-    CRITICAL_START();
-    klog_print("vmSupport: Avvio lettura da flash della nuova pagina (page-in)...\n");
-    
-    read_or_write_flash(victim, page_tbl_index, asid, FLASHREAD);
+    read_or_write_flash(victim, p, asid, FLASHREAD);
    
     klog_print("vmSupport: Page-in completato.\n");
-    update_swap_pool_entry(victim, page_tbl_index, asid, pte_p);
+    update_swap_pool_entry(victim, p, asid, pte_p);
 
-    unsigned int pfn = (FRAMEPOOLSTART + (victim * PAGESIZE)) >> VPNSHIFT;
+    unsigned int pfn = (FRAMEPOOLSTART + (victim * PAGESIZE)) >> ENTRYLO_PFN_BIT;
 
 /* * CORREZIONE DEFINITIVA:
  * Costruiamo pte_entryLO da zero in un unico passaggio.
  * Questo è il modo corretto e robusto: combina il PFN (riportato a indirizzo base),
  * il bit di validità e il bit di "dirty".
  */
-pte_p->pte_entryLO = (pfn << VPNSHIFT) | VALIDON | DIRTYON;
+    swap_pool_table[victim].sw_pte->pte_entryLO = (pfn << ENTRYLO_PFN_BIT) | VALIDON | DIRTYON;
 
-updateTLB(pte_p);
-CRITICAL_END();
+    updateTLB(swap_pool_table[victim].sw_pte);
     klog_print("vmSupport: Strutture aggiornate.\n");
-
     releaseMutex(&swap_pool_sem, pid);
     klog_print("vmSupport: --- Fine Pager. Ritorno al processo. ---\n");
     LDST(current_state);
@@ -105,18 +74,20 @@ CRITICAL_END();
 
 void read_or_write_flash(int frame_i, int vpn, int asid, int op) {
     unsigned int frame_phys = FRAMEPOOLSTART + (frame_i * PAGESIZE);
+    int dev_index = findDeviceIndex((memaddr*)frame_phys);
+    int pid = SYSCALL(GETPROCESSID, 0, 0, 0);
+    getMutex(&sharable_dev_sem[dev_index], pid);
+
     dtpreg_t* flash = (dtpreg_t*)DEV_REG_ADDR(IL_FLASH, asid - 1);
     flash->data0 = frame_phys;
     unsigned int cmd = ((unsigned int)vpn << 8) | op;
 
     int status = SYSCALL(DOIO, (int)&(flash->command), cmd, 0);
-
+    releaseMutex(&sharable_dev_sem[dev_index], pid);
     if (status != 1) { // 1 == READY
         support_t* support = (support_t*)SYSCALL(GETSUPPORTPTR, 0, 0, 0);
         state_t* exp_state = &(support->sup_exceptState[GENERALEXCEPT]);
         programTrapHandler(exp_state);
-    } else {
-        //klog_print("  flash_io: Operazione I/O su flash completata con successo.\n");
     }
 }
 
